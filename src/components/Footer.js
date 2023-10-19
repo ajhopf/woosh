13a5780ffba5e5e3c37d2fb0644e266a4c118615
André Hopf <andre.hopf@gmail.com>
2023-10-18 22:07:32 -0300

    Ajustes no footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,42 @@
 import './Footer.scss'
 
-import instagram from '../assets/instagram.svg'
 import {Link} from "react-router-dom";
 
+import wooshIcon from '../assets/icon-studio.png';
+import instagramIcon from '../assets/instagram_icon.png';
+import whatsappIcon from '../assets/whatsapp_icon.png';
+import vimeoIcon from '../assets/vimeo_icon.png';
 
 const Footer = () => {
-  return <footer className="d-flex align-items-center justify-content-center">
-    <Link to="https://www.instagram.com/bgelpi/" target={"_blank"}>
-      <img src={instagram}/>
-    </Link>
+  return <footer className="d-grid">
+    <div className="left-div ps-3">
+      <p className="titles">{ "Reach out".toUpperCase() }</p>
+      <img className="woosh-footer-icon" src={ wooshIcon } alt="Woosh Studio Icon"/>
+    </div>
+    <div className="right-div pe-3">
+      <p>If you're interested in learning more about our love for music and innovative ideas, don't hesitate to reach out.</p>
+      <div>
+        <p>bgelpi@woosh.com</p>
+        <p>+55 51 99423-1763</p>
+      </div>
+      <ul className="social-media">
+        <li>
+          <Link to="https://www.instagram.com/bgelpi/" target={"_blank"} aria-label="Bolivar Gelpi's Instagram">
+            <img src={instagramIcon} alt="Instagram logo"/>
+          </Link>
+        </li>
+        <li>
+          <Link to="https://vimeo.com/wooshaudio" target={"_blank"} aria-label="Link to Woosh Vimeo Channel">
+            <img className="vimeo" src={vimeoIcon} alt="Vimeo logo"/>
+          </Link>
+        </li>
+        <li>
+          <Link to="https://wa.me/5551994231763" target={"_blank"}  aria-label="Chat on WhatsApp">
+            <img src={whatsappIcon} alt="Youtube logo"/>
+          </Link>
+        </li>
+      </ul>
+    </div>
   </footer>
 };
 
